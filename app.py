4f05ff34b0948788c750307327c4ced7c6281392
Stepan_Shavalyuk <93962006+BigPaPka@users.noreply.github.com>
2024-06-18 15:48:18 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-import pickle
+
 # Создание экземпляра приложения Flask
 app = Flask(__name__)
 # Загрузка модели из файла
@@ -18,9 +17,13 @@ def predict():
     rooms = int(request.form['rooms'])
     area = int(request.form['area'])  # Общая площадь
     kitchen_area = int(request.form['kitchen_area'])  # Площадь кухни
+    floor = int(request.form['floor'])
+    minutes_to_metro = int(request.form['minutes_to_metro'])
+    renovation_type = request.form['renovation_type']
+    apartment_type = request.form['apartment_type']
 
     # Выполнение предсказания с использованием модели
-    prediction = model1.predict([[rooms, area, kitchen_area]])
+    prediction = model1.predict([[rooms, area, kitchen_area, floor, minutes_to_metro,renovation_type,apartment_type]])
 
     # Округление предсказания до двух знаков после запятой
     output = round(prediction[0], 2)
@@ -32,3 +35,5 @@ def predict():
 # Запуск приложения
 if __name__ == "__main__":
     app.run(debug=True)  # Включаем режим отладки для разработки
+
+
